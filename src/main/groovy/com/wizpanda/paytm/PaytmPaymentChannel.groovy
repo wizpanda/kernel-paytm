6e48876a02e63b15153bd0ef715b74de16fcc15f
Shashank Agrawal <shashank@wizpanda.com>
2020-05-16 01:30:27 +0530

    Trying to fix the build issue with @CompileStatic

--- src/main/groovy/com/wizpanda/paytm/PaytmPaymentChannel.groovy
+++ src/main/groovy/com/wizpanda/paytm/PaytmPaymentChannel.groovy
@@ -1,10 +1,13 @@
 package com.wizpanda.paytm
 
+import groovy.transform.CompileStatic
+
 /**
  * Common class to hold additional data which need to be send to payTm for transaction.
  *
  * @author Shashank Agrawal
  */
+@CompileStatic
 enum PaytmPaymentChannel {
 
     WEB,
